d11640d11619e478c06c2db03c941b5715d237a4
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-22 14:39:12 +0100

    adding add log action and reducer

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React,{useState} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {addLog} from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({addLog}) => {
     const [message, setMessage] = useState('');
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState('');
@@ -11,8 +14,15 @@ const AddLogModal = () => {
             M.toast({html: 'Please enter a message and tech'});
         }
         else{
-            console.log(message, tech, attention);
-            //clear fields
+            const newLog = {
+                message,
+                attention,
+                tech,
+                date: new Date()
+            }
+
+            addLog(newLog);
+            M.toast({html: `Log added by ${tech}`})
             setMessage('');
             setTech('');
             setAttention(false);
@@ -88,4 +98,8 @@ const modalStyle = {
     height: "75%"
 };
 
-export default AddLogModal
+AddLogModal.propTypes = {
+    addLog: PropTypes.func.isRequired
+}
+
+export default connect(null, {addLog})(AddLogModal)
