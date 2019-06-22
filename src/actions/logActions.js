6ed6044c6880b071d28c89bdf1c157bba0f664ac
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-22 14:12:58 +0100

    mapping state to logs compt and firing actions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -12,13 +12,14 @@ import {GET_LOGS, SET_LOADING, LOGS_ERROR} from './types';
 //     }
 // };
 
+// Get Logs from server
 export const getLogs = () => async dispatch => {
     try{
         setLoading();
 
         const res = await fetch('/logs');
         const data = await res.json();
-        
+
         dispatch({
             type: GET_LOGS,
             payload: data
